97382d4891fa4b5ceb1a99052bc7a59b43e50c57
Asia Antczak <aasiaantczak@gmail.com>
2018-06-20 13:27:51 +0100

    Fix feature tests

--- lib/account.rb
+++ lib/account.rb
@@ -3,7 +3,7 @@ require_relative './statement'
 require_relative './transaction_log'
 
 class Account # :nodoc:
-  attr_reader :account_balance
+  attr_reader :account_balance, :transaction_log
 
   def initialize(transaction_log = TransactionLog.new)
     @account_balance = 0
@@ -12,14 +12,14 @@ class Account # :nodoc:
 
   def make_deposit(amount, transaction = Transaction.new)
     update_credit_balance(amount)
-    t = transaction.create_credit_transaction(amount, @account_balance)
-    @transaction_log.save_transaction(t)
+    t = transaction.create_credit_transaction(amount, account_balance)
+    transaction_log.save_transaction(t)
   end
 
   def withdraw(amount, transaction = Transaction.new)
     update_debit_balance(amount)
-    t = transaction.create_debit_transaction(amount, @account_balance)
-    @transaction_log.save_transaction(t)
+    t = transaction.create_debit_transaction(amount, account_balance)
+    transaction_log.save_transaction(t)
   end
 
   def print_account_statement(statement = Statement.new)
@@ -29,7 +29,7 @@ class Account # :nodoc:
   private
 
   def reverse_transactions_order
-    @transaction_log.transactions_list.reverse
+    transaction_log.transactions_list.reverse
   end
 
   def update_credit_balance(amount)
--- spec/feature_tests.rb
+++ spec/feature_tests.rb
@@ -18,14 +18,14 @@ def make_deposit_on_account
   account = Account.new
   transaction_log = TransactionLog.new
   account.make_deposit(30)
-  p transaction_log.transactions_list
+  p account.transaction_log.transactions_list
 end
 
 def withdraw_from_account
   account = Account.new
   transaction_log = TransactionLog.new
   account.withdraw(30)
-  p account[:transaction_log]
+  p account.transaction_log.transactions_list
 end
 
 def print_account_statement
